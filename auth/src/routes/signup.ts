9c22e37610effa4b258c67cd0cbf9098355e8d3a
meathirarose <athirarosejohn@gmail.com>
2024-11-14 16:40:14 +0530

    feat(auth): complete signup backend with JWT and MongoDB integration

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { User } from "../models/user";
+import jwt  from "jsonwebtoken";
 
+import { User } from "../models/user";
 import { RequestValidatonError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 
@@ -35,6 +36,20 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    //generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      }, 
+      process.env.JWT_KEY!
+    );
+
+    //store it on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
