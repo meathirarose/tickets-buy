67b3f493d6c2b049ee40108f60cd9e1e5f96d0ff
meathirarose <athirarosejohn@gmail.com>
2024-11-14 19:28:28 +0530

    feat(auth): add current user middleware for attaching user data from JWT

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,25 +1,11 @@
- import express from 'express';
- import jwt from 'jsonwebtoken';
+import express from 'express';
+
+import { currentUser } from '../middlewares/current-user';
 
  const router = express.Router();
 
- router.get('/api/users/currentuser', (req, res)=>{
-   if(!req.session?.jwt){
-      res.send({currentUser: null});
-      return;
-   }
-    
-   try {
-      const payload = jwt.verify(
-         req.session.jwt,
-         process.env.JWT_KEY!
-      );
-      res.send({ currentUser: payload });
-
-   } catch (error) {
-      res.send({ currentUser: null })
-   }
-
+ router.get('/api/users/currentuser', currentUser, (req, res) => {   
+   res.send({ currentUser: req.currentUser || null });
  });
 
  export { router as currentUserRouter };
